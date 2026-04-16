f65d93e0bf61f6dc5a42ea6d54e253ae328f591f
aalexgonzaalez <alejandro.gonzalezcarrasco9@gmail.com>
2026-04-16 21:12:54 +0200

    Primera versión de la función que actualiza biomarcadores

--- agents/paciente.py
+++ agents/paciente.py
@@ -5,8 +5,9 @@ class Paciente(mesa.Agent):
     '''Agente que representa a un paciente con factores de riesgo cardiovascular.'''
 
     def __init__(self, unique_id, model, grupo, es_mujer, edad, abandona, semana_abandono,
-                 calorias_base, pct_prot_base, pct_carb_base, pct_grasa_base,
-                 calorias, pct_prot, pct_carb, pct_grasa):
+                 calorias_base, pct_prot_base, pct_carb_base, pct_grasa_base, pct_grasa_sat_base, pct_grasa_mono_base, pct_grasa_poli_base,
+                 calorias, pct_prot, pct_carb, pct_grasa, pct_grasa_sat, pct_grasa_mono, pct_grasa_poli,
+                 col_total, col_ldl):
         super().__init__(model)
 
         # Perfil del paciente
@@ -24,18 +25,28 @@ class Paciente(mesa.Agent):
         self.pct_prot_base = pct_prot_base
         self.pct_carb_base = pct_carb_base
         self.pct_grasa_base = pct_grasa_base
+        self.pct_grasa_sat_base = pct_grasa_sat_base
+        self.pct_grasa_mono_base = pct_grasa_mono_base
+        self.pct_grasa_poli_base = pct_grasa_poli_base
 
         # Calorías (kcal) y macronuientes esenciales OBJETIVO (PORCENTAJES)
         self.calorias_objetivo = calorias
         self.pct_prot_objetivo = pct_prot
         self.pct_carb_objetivo = pct_carb
         self.pct_grasa_objetivo = pct_grasa
+        self.pct_grasa_sat_objetivo = pct_grasa_sat
+        self.pct_grasa_mono_objetivo = pct_grasa_mono
+        self.pct_grasa_poli_objetivo = pct_grasa_poli
+
+        # Variables de salud iniciales
+        self.col_total = col_total
+        self.col_ldl = col_ldl
 
     def comer(self):
         '''Simula la ingesta semanal del paciente'''
 
-        # Asumimos que el paciente tiene una desviación del 5% respecto a su objetivo
-        VARIABILIDAD_SEMANAL = 0.05
+        # Asumimos que el paciente tiene una desviación del 2,5% respecto a su objetivo
+        VARIABILIDAD_SEMANAL = 0.025
 
         # Comprobar abandono: ¿En qué semana estamos?
         if self.abandona and self.model.current_step >= self.semana_abandono:
@@ -44,6 +55,9 @@ class Paciente(mesa.Agent):
             self.pct_prot_objetivo = self.pct_prot_base
             self.pct_carb_objetivo = self.pct_carb_base
             self.pct_grasa_objetivo = self.pct_grasa_base
+            self.pct_grasa_sat_objetivo = self.pct_grasa_sat_base
+            self.pct_grasa_mono_objetivo = self.pct_grasa_mono_base
+            self.pct_grasa_poli_objetivo = self.pct_grasa_poli_base
             estado = "ABANDONO"
 
         else:
@@ -69,12 +83,68 @@ class Paciente(mesa.Agent):
         self.carbohidratos = (self.calorias * carbohidratos_pct_real) / 4.0
         self.grasas = (self.calorias * grasas_pct_real) / 9.0 
 
+        # 5. Hacemos lo mismo para los tipos de grasa (saturada, monoinsaturada, poliinsaturada)
+        raw_sat = max(0.01, np.random.normal(self.pct_grasa_sat_objetivo, self.pct_grasa_sat_objetivo * VARIABILIDAD_SEMANAL))
+        raw_mono = max(0.01, np.random.normal(self.pct_grasa_mono_objetivo, self.pct_grasa_mono_objetivo * VARIABILIDAD_SEMANAL))
+        raw_poli = max(0.01, np.random.normal(self.pct_grasa_poli_objetivo, self.pct_grasa_poli_objetivo * VARIABILIDAD_SEMANAL))
+
+        suma_tipos_grasa = raw_sat + raw_mono + raw_poli
+        self.grasa_sat_real = raw_sat / suma_tipos_grasa
+        self.grasa_mono_real = raw_mono / suma_tipos_grasa
+        self.grasa_poli_real = raw_poli / suma_tipos_grasa
+
+        self.grasa_sat = self.grasas * self.grasa_sat_real
+        self.grasa_mono = self.grasas * self.grasa_mono_real
+        self.grasa_poli = self.grasas * self.grasa_poli_real
+
         return estado # Devuelve el estado del paciente (activo o abandono)
 
     def actualizar_biomarcadores(self):
         '''Función para actualizar los biomarcadores de salud del paciente en función de su ingesta real'''
-        # Aquí se implementaría la lógica para actualizar los biomarcadores (colesterol total, LDL, HDL, etc.)
-        pass
+        
+        # 1. Guardamos el colesterol LDL con el que parte el paciente
+        if not hasattr(self, 'col_ldl_base'):
+            self.col_ldl_base = self.col_ldl
+
+        # Usamos nueva variable para no cambiar valor
+        col_ldl_inicial = self.col_ldl_base
+
+        # 2. Comparar porcentajes de energía de grasas (Ingesta actual vs Habitos base)
+        pct_sat_actual = (self.grasa_sat * 9.0) / self.calorias
+        pct_mono_actual = (self.grasa_mono * 9.0) / self.calorias
+        pct_poli_actual = (self.grasa_poli * 9.0) / self.calorias
+
+        # Calculamos la mejora en términos de porcentajes respecto a los hábitos base
+        # Bajar saturada y subir monoinsaturada y poliinsaturada es beneficioso
+        mejora_sat = self.pct_grasa_sat_base - pct_sat_actual
+        mejora_mono = pct_mono_actual - self.pct_grasa_mono_base
+        mejora_poli = pct_poli_actual - self.pct_grasa_poli_base
+
+        # 3. Puntuamos la mejora total 
+        puntuacion_grasas = (mejora_sat * 1.5) + (mejora_mono * 0.3) + (mejora_poli * 0.7)
+        calidad_grasas = max(0.0, min(1.0, puntuacion_grasas / 0.06)) # Normalizamos entre 0 y 1
+
+        # 4. Impacto del déficit calórico (+déficit = +mejoría)
+        deficit = self.calorias_base - self.calorias
+        factor_calorias = max(0.0, min(1.0, deficit / 700.0)) # Un déficit de 700 kcal da puntuación máxima (1.0)
+
+        # 5. Combinamos ambos factores para calcular la mejora total
+        mejora_total = (calidad_grasas * 0.8) + (factor_calorias * 0.2) # Damos más peso a la calidad de las grasas que al déficit calórico
+
+        # 6. Establecemos un límite de reducción del colesterol LDL (máximo 13% de mejora respecto al valor inicial)
+        mejora_max_ldl = col_ldl_inicial * 0.13
+        ldl_objetivo = col_ldl_inicial - (mejora_total * mejora_max_ldl)
+
+        # 7. Función asíntotica (curva más pronunciada al principio y luego se estabiliza)
+        if ldl_objetivo < self.col_ldl:
+            # El paciente mejora rápido al principio y luego se estabiliza
+            k = 0.35
+        else:
+            # Si el paciente empeora, el aumento es más gradual
+            k = 0.15
+        
+        self.col_ldl = self.col_ldl + (ldl_objetivo - self.col_ldl) * k
+
 
     def step(self):
         '''Función que se ejecuta en cada paso (semana) de la simulación'''
@@ -83,18 +153,28 @@ class Paciente(mesa.Agent):
         estado_paciente = self.comer()
 
         # Actualizar los biomarcadores de salud en función de la ingesta 
-        # self.actualizar_biomarcadores()
+        self.actualizar_biomarcadores()
 
         # Prueba con 5 pacientes para analizar su evolución semanal de ingesta
-        if self.unique_id < 30:
+        if self.unique_id < 5:
+            # Porcentajes de macronutrientes consumidos cada semana
             pct_proteinas = (self.proteinas * 4 / self.calorias) * 100
             pct_carbohidratos = (self.carbohidratos * 4 / self.calorias) * 100
             pct_grasas = (self.grasas * 9 / self.calorias) * 100
+
+            # Porcentajes de tipos de grasa consumidos cada semana
+            pct_sat = (self.grasa_sat / self.grasas) * 100
+            pct_mono = (self.grasa_mono / self.grasas) * 100
+            pct_poli = (self.grasa_poli / self.grasas) * 100
+
             print(f'    • Paciente {self.unique_id} ({self.grupo}) [{estado_paciente}] -> '
-                    f'Calorías: {self.calorias:.1f} kcal, '
-                    f'Proteínas: {self.proteinas:.1f}g ({pct_proteinas:.1f}%), '
-                    f'Carbohidratos: {self.carbohidratos:.1f}g ({pct_carbohidratos:.1f}%), '
-                    f'Grasas: {self.grasas:.1f}g ({pct_grasas:.1f}%)')
+                    f'Calorías: {self.calorias:.1f} kcal | '
+                    f'Macros: Prot -> {self.proteinas:.1f}g ({pct_proteinas:.1f}%), '
+                    f'Carb: {self.carbohidratos:.1f}g ({pct_carbohidratos:.1f}%), ' 
+                    f'Grasas: {self.grasas:.1f}g ({pct_grasas:.1f}%) '
+                    f'[Sat: {self.grasa_sat:.1f}g ({pct_sat:.1f}%), Mono: {self.grasa_mono:.1f}g ({pct_mono:.1f}%), Poli: {self.grasa_poli:.1f}g ({pct_poli:.1f}%)] | '
+                    f'Colesterol: {self.col_total:.1f} mmol/L | '
+                    f'LDL: {self.col_ldl:.1f} mmol/L')
 
         # # Simulación completa de ingesta semanal
         # pct_proteinas = (self.proteinas * 4 / self.calorias) * 100
--- model/cardioModel.py
+++ model/cardioModel.py
@@ -33,17 +33,28 @@ class CardioModel(mesa.Model):
                 semana_abandono = random.randint(1, 12) if abandona else None # Abandona en alguna semana entre la 1 y la 12
 
                 # HÁBITOS BASE (Lo que comía antes de empezar la dieta)
-                calorias_base = max(1000, np.random.normal(settings.ENERGIA_MEDIA_MED_INICIAL, settings.ENERGIA_STD_MED_INICIAL))
+                calorias_base = max(1200, np.random.normal(settings.ENERGIA_MEDIA_MED_INICIAL, settings.ENERGIA_STD_MED_INICIAL))
                 pct_prot_base = np.random.normal(settings.PROT_PCT_MEDIA_MED_INICIAL, settings.PROT_PCT_STD_MED_INICIAL)
                 pct_carb_base = np.random.normal(settings.CARB_PCT_MEDIA_MED_INICIAL, settings.CARB_PCT_STD_MED_INICIAL)
                 pct_grasa_base = np.random.normal(settings.GRASA_PCT_MEDIA_MED_INICIAL, settings.GRASA_PCT_STD_MED_INICIAL)
+                pct_grasa_sat_base = max(0.01, np.random.normal(settings.GRASA_SAT_PCT_MEDIA_MED_INICIAL, settings.GRASA_SAT_PCT_STD_MED_INICIAL))
+                pct_grasa_mono_base = max(0.01, np.random.normal(settings.GRASA_MONO_PCT_MEDIA_MED_INICIAL, settings.GRASA_MONO_PCT_STD_MED_INICIAL))
+                pct_grasa_poli_base = max(0.01, np.random.normal(settings.GRASA_POLI_PCT_MEDIA_MED_INICIAL, settings.GRASA_POLI_PCT_STD_MED_INICIAL)) 
 
 
                 # OBJETIVOS DE LA DIETA MEDITERRÁNEA
-                calorias = max(1000, np.random.normal(settings.ENERGIA_MEDIA_MED, settings.ENERGIA_STD_MED))
+                deficit_med = max(100, np.random.normal(settings.DEFICIT_MEDIA_MED, 200))
+                calorias = calorias_base - deficit_med
                 pct_prot = np.random.normal(settings.PROT_PCT_MEDIA_MED, settings.PROT_PCT_STD_MED)
                 pct_carb = np.random.normal(settings.CARB_PCT_MEDIA_MED, settings.CARB_PCT_STD_MED)
                 pct_grasa = np.random.normal(settings.GRASA_PCT_MEDIA_MED, settings.GRASA_PCT_STD_MED)
+                pct_grasa_sat = max(0.01, np.random.normal(settings.GRASA_SAT_PCT_MEDIA_MED, settings.GRASA_SAT_PCT_STD_MED))
+                pct_grasa_mono = max(0.01, np.random.normal(settings.GRASA_MONO_PCT_MEDIA_MED, settings.GRASA_MONO_PCT_STD_MED))
+                pct_grasa_poli = max(0.01, np.random.normal(settings.GRASA_POLI_PCT_MEDIA_MED, settings.GRASA_POLI_PCT_STD_MED))
+
+                # Variables de salud iniciales basadas en Campana de Gauss
+                col_total = np.random.normal(settings.COL_TOTAL_MEDIA_MED, settings.COL_TOTAL_STD_MED)
+                col_ldl = np.random.normal(settings.COL_LDL_MEDIA_MED, settings.COL_LDL_STD_MED)
 
 
             # Grupo dieta baja en grasas
@@ -57,31 +68,38 @@ class CardioModel(mesa.Model):
                 semana_abandono = random.randint(1, 12) if abandona else None # Abandona en alguna semana entre la 1 y la 12
 
                 # HÁBITOS BASE (Lo que comía antes de empezar la dieta)
-                calorias_base = max(1000, np.random.normal(settings.ENERGIA_MEDIA_LOW_FAT_INICIAL, settings.ENERGIA_STD_LOW_FAT_INICIAL))
+                calorias_base = max(1200, np.random.normal(settings.ENERGIA_MEDIA_LOW_FAT_INICIAL, settings.ENERGIA_STD_LOW_FAT_INICIAL))
                 pct_prot_base = np.random.normal(settings.PROT_PCT_MEDIA_LOW_FAT_INICIAL, settings.PROT_PCT_STD_LOW_FAT_INICIAL)
                 pct_carb_base = np.random.normal(settings.CARB_PCT_MEDIA_LOW_FAT_INICIAL, settings.CARB_PCT_STD_LOW_FAT_INICIAL)
                 pct_grasa_base = np.random.normal(settings.GRASA_PCT_MEDIA_LOW_FAT_INICIAL, settings.GRASA_PCT_STD_LOW_FAT_INICIAL)
+                pct_grasa_sat_base = max(0.01, np.random.normal(settings.GRASA_SAT_PCT_MEDIA_LOW_FAT_INICIAL, settings.GRASA_SAT_PCT_STD_LOW_FAT_INICIAL))
+                pct_grasa_mono_base = max(0.01, np.random.normal(settings.GRASA_MONO_PCT_MEDIA_LOW_FAT_INICIAL, settings.GRASA_MONO_PCT_STD_LOW_FAT_INICIAL))
+                pct_grasa_poli_base = max(0.01, np.random.normal(settings.GRASA_POLI_PCT_MEDIA_LOW_FAT_INICIAL, settings.GRASA_POLI_PCT_STD_LOW_FAT_INICIAL))
 
                 # OBJETIVOS DE LA DIETA BAJA EN GRASAS
-                calorias = max(1000, np.random.normal(settings.ENERGIA_MEDIA_LOW_FAT, settings.ENERGIA_STD_LOW_FAT))
+                deficit_low_fat = max(100, np.random.normal(settings.DEFICIT_MEDIA_LOW_FAT, 100))
+                calorias = calorias_base - deficit_low_fat
                 pct_prot = np.random.normal(settings.PROT_PCT_MEDIA_LOW_FAT, settings.PROT_PCT_STD_LOW_FAT)
                 pct_carb = np.random.normal(settings.CARB_PCT_MEDIA_LOW_FAT, settings.CARB_PCT_STD_LOW_FAT)
                 pct_grasa = np.random.normal(settings.GRASA_PCT_MEDIA_LOW_FAT, settings.GRASA_PCT_STD_LOW_FAT)
+                pct_grasa_sat = max(0.01, np.random.normal(settings.GRASA_SAT_PCT_MEDIA_LOW_FAT, settings.GRASA_SAT_PCT_STD_LOW_FAT))
+                pct_grasa_mono = max(0.01, np.random.normal(settings.GRASA_MONO_PCT_MEDIA_LOW_FAT, settings.GRASA_MONO_PCT_STD_LOW_FAT))
+                pct_grasa_poli = max(0.01, np.random.normal(settings.GRASA_POLI_PCT_MEDIA_LOW_FAT, settings.GRASA_POLI_PCT_STD_LOW_FAT))
 
-                # # Variables de salud iniciales basadas en Campana de Gauss
-                # col_total = np.random.normal(settings.COL_TOTAL_MEDIA_LOW_FAT, settings.COL_TOTAL_STD_LOW_FAT)
-                # col_ldl = np.random.normal(settings.COL_LDL_MEDIA_LOW_FAT, settings.COL_LDL_STD_LOW_FAT)
+                # Variables de salud iniciales basadas en Campana de Gauss
+                col_total = np.random.normal(settings.COL_TOTAL_MEDIA_LOW_FAT, settings.COL_TOTAL_STD_LOW_FAT)
+                col_ldl = np.random.normal(settings.COL_LDL_MEDIA_LOW_FAT, settings.COL_LDL_STD_LOW_FAT)
 
 
             # Instanciar el agente Paciente con sus características
             paciente = Paciente(i, self, grupo, es_mujer, edad, abandona, semana_abandono,
-                                calorias_base, pct_prot_base, pct_carb_base, pct_grasa_base,
-                                calorias, pct_prot, pct_carb, pct_grasa)
+                                calorias_base, pct_prot_base, pct_carb_base, pct_grasa_base, pct_grasa_sat_base, pct_grasa_mono_base, pct_grasa_poli_base,
+                                calorias, pct_prot, pct_carb, pct_grasa, pct_grasa_sat, pct_grasa_mono, pct_grasa_poli,
+                                col_total, col_ldl)
             self.schedule.add(paciente)
     
     def step(self):
         '''Función que se ejecuta en cada paso de la simulación.'''
-        # self.datacollector.collect(self)
         print(f"\n--- Iniciando Semana {self.current_step} ---")
         self.schedule.step()
         self.current_step += 1
